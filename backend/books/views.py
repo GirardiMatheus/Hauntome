f7a91e71959c6b491777467043a0bfe8f5f7ea4c
Matheus Girardi <girardimatheus27@gmail.com>
2025-03-06 14:07:46 -0300

    feat(books): adiciona permissões personalizadas e ajustes nas views

--- backend/books/views.py
+++ backend/books/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Book.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
