fc41941ab30c3d924f35eed26fb61292cab6cfc5
Matheus Girardi <girardimatheus27@gmail.com>
2025-03-07 11:03:05 -0300

    fix: fixes router configuration and permissions for DELETE, PUT and PATCH methods

--- backend/api/urls/book_urls.py
+++ backend/api/urls/book_urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from api.views.books_views import BookViewSet
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'books', BookViewSet)  
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  
 ]
